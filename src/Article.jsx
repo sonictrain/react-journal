0d75ff015427b63aa7f1fec45e193769db31daca
sonictrain <nicola.brucoli92@gmail.com>
2022-10-04 20:26:28 +0100

    setting up flexbox containers

--- src/Article.jsx
+++ src/Article.jsx
@@ -4,18 +4,21 @@ function Article() {
     return (
         <article>
             <div className="article--cover"></div>
-            <div className="article--info.container">
-                <div className="article--location">
-                    <img src="/location-pin.svg" alt="pin icon"></img>
+            <div className="article--info--container">
+                <div className="article--location--container">
+                    <img className="location--pin" src="/location-pin.svg" alt="pin icon"></img>
                     <h4>Japan</h4>
-                    <p className="location--link"></p>
+                    <p className="location--link">View on Google Maps</p>
                 </div>
                 <div className="article--info">
-
+                    <h2>Monte Fuji</h2>
+                    <h5>12 Jan, 2021 - 24 Jan, 2021</h5>
+                    <p>Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
                 </div>
             </div>
         </article>
     )
 }
+
 
 export default Article
--- src/Header.jsx
+++ src/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Header() {
     return (
         <nav className="header">
-            <img className="header--logo" src="/globe_logo.svg" alt="Globe logo" />
+            <img className="header--logo" src="/globe-logo.svg" alt="Globe logo" />
             <h3 className="header--title">my travel journal.</h3>
         </nav>
     )
