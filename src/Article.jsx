9ebe13a8f7f2ac207041dfdc3b37c085f47bf1da
sonictrain <nicola.brucoli92@gmail.com>
2022-10-10 00:02:49 +0100

    fixed rem units and resizing behaviour. Set up data structure for data.js

--- src/Article.jsx
+++ src/Article.jsx
@@ -3,17 +3,19 @@ import React from 'react'
 function Article(props) {
     return (
         <article>
-            <img src={props.img} alt={props.img_alt} />
-            <div className="article--info--container">
-                <div className="article--location--container">
-                    <img className="location--pin" src="/location-pin.svg" alt="pin icon"></img>
-                    <h4 className="location--city">{props.location}</h4>
-                    <a href={props.maps_link} className="location--link">View on Google Maps</a>
-                </div>
-                <div className="article--info">
-                    <h2 className="article--title">{props.title}</h2>
-                    <h5 className="article--date">{props.from_date} - {props.to_date}</h5>
-                    <p className="article--description">{props.description}</p>
+            <div className="divider">    
+                <img className="article-img" src={props.img} alt={props.img_alt} />
+                <div className="article-container">
+                    <div className="location-container">
+                        <img className="pin" src="/location-pin.svg" alt="pin icon"></img>
+                        <h4 className="city">{props.location}</h4>
+                        <a href={props.maps_link} className="maps-link">View on Google Maps</a>
+                    </div>
+                    <div className="detail-container">
+                        <h2 className="title">{props.title}</h2>
+                        <h5 className="date-range">{props.from_date} - {props.to_date}</h5>
+                        <p className="description">{props.description}</p>
+                    </div>
                 </div>
             </div>
         </article>
